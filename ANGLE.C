a329725d4fab1e4290d37a9dd8254d06d337a06d
purvisharadadiya <purva.radadiya3110@gmail.com>
2024-11-30 14:04:21 +0530

    pr-1

---
+++ ANGLE.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+main()
+{
+    int angle1=65,angle2=45,angle3;
+    clrscr();
+    angle3=180-(65+45);
+    printf("the third agle of the %d\n",angle3);
+    getch();
+}
+
+
+
+
+
+
+
---
+++ BASE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+     int bs,hra,da,ta,ans;
+     clrscr();
+     printf(anter gross salary);
+     scanf("%d",&bs);
+     bs=(bs*10)/100;
+     bs=(bs*5)/100;
+     bs=(bs*8)/100;
+     ans=hra+da+ta;
+     printf("gross salary %d",bs);
+     getch();
+
+}
